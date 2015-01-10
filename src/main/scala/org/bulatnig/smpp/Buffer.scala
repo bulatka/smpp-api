d4666c004239f264c73a72809674387b10aae04b
Bulat Nigmatullin <bulatnig@gmail.com>
2015-01-10 12:13:51 +0300

    smpp Buffer implementation complete

--- src/main/scala/org/bulatnig/smpp/Buffer.scala
+++ src/main/scala/org/bulatnig/smpp/Buffer.scala
@@ -1,87 +1,108 @@
 package org.bulatnig.smpp
 
-import java.nio.charset.StandardCharsets
+import java.nio.charset.{Charset, StandardCharsets}
 
 import scala.collection.mutable.ArrayBuffer
 
-class Buffer(val buffer: ArrayBuffer[Byte]) {
+class Buffer {
 
-  var cursor = 0
+  val buffer = new ArrayBuffer[Byte]()
 
-  def this() {
-    this(new ArrayBuffer[Byte]())
-  }
+  private var position = 0
 
-  def this(array: Array[Byte]) {
-    this(array.to[ArrayBuffer])
+  def this(array: TraversableOnce[Byte]) {
+    this()
+    buffer ++= array
   }
 
   def toArray = buffer.toArray
 
   def length = buffer.length
 
-  def ++=(xs: Array[Byte]) {
+  def ++=(xs: TraversableOnce[Byte]) = {
     buffer ++= xs
     this
   }
 
-  def +=(elem: Byte) {
-    buffer += elem
-    this
-  }
-
-  def +=(elem: Short) {
-    buffer += (elem >>> 8).toByte
+  def appendByte(elem: Int) = {
     buffer += elem.toByte
     this
   }
 
-  def +=(elem: Int) {
-    buffer += (elem >>> 24).toByte
-    buffer += (elem >>> 16).toByte
-    buffer += (elem >>> 8).toByte
+  def appendShort(elem: Int) = {
+    buffer += (elem >> 8).toByte
     buffer += elem.toByte
     this
   }
 
-  def +=(elem: Long) {
-    this += elem.toInt
+  def appendInt(elem: Int) = {
+    buffer += (elem >> 24).toByte
+    buffer += (elem >> 16).toByte
+    buffer += (elem >> 8).toByte
+    buffer += elem.toByte
     this
   }
 
-  def addCString(elem: String) {
+  def appendCString(elem: String) = {
     if (elem != null) {
       this ++= elem.getBytes(StandardCharsets.US_ASCII)
     }
-    this += Buffer.ZERO
+    buffer += Buffer.NULL
     this
   }
 
-  def addString(elem: String) {
-    addString(elem, StandardCharsets.US_ASCII.name())
-    this
-  }
-
-  def addString(elem: String, charsetName: String) {
+  def appendString(elem: String, charset: Charset) = {
     if (elem != null) {
-      this ++= elem.getBytes(charsetName)
+      this ++= elem.getBytes(charset)
     }
     this
   }
 
-  def readByte(): Byte = {
-    val result = buffer(cursor)
-    cursor += 1
+  def readByte(): Int = {
+    val result = buffer(position) & 0xFF
+    position += 1
     result
   }
 
-  def readShort() {
-    var result: Short = buffer(cursor)
-    cursor += 1
-    result <<= 8
-    result |= buffer(cursor)
-    cursor += 1
+  def readShort(): Int = {
+    val result = ((buffer(position) & 0xFF) << 8) | (buffer(position + 1) & 0xFF)
+    position += 2
     result
+  }
+
+  def readInt(): Int = {
+    val result = (buffer(position) << 24) |
+      ((buffer(position + 1) & 0xFF) << 16) |
+      ((buffer(position + 2) & 0xFF) << 8) |
+      (buffer(position + 3) & 0xFF)
+    position += 4
+    result
+  }
+
+  def readCString(): String = {
+    val idx = buffer.indexOf(Buffer.NULL, position)
+    if (idx == -1) throw new IndexOutOfBoundsException("C-Octet String termination not found")
+    var result: String = null
+    if (idx > position) {
+      val bytes = new Array[Byte](idx - position)
+      Array.copy(buffer.toArray, position, bytes, 0, bytes.length)
+      result = new String(bytes, StandardCharsets.US_ASCII)
+    }
+    position = idx + 1
+    result
+  }
+
+  def readString(length: Int, charset: Charset): String = {
+    val bytes = new Array[Byte](length)
+    Array.copy(buffer.toArray, position, bytes, 0, bytes.length)
+    val result = new String(bytes, charset)
+    position += length
+    result
+  }
+
+  def rewind() = {
+    position = 0
+    this
   }
 
   def toHexString = buffer.map("%02X" format _).mkString
@@ -89,5 +110,5 @@ class Buffer(val buffer: ArrayBuffer[Byte]) {
 }
 
 object Buffer {
-  private val ZERO: Byte = 0
+  private val NULL: Byte = 0
 }
--- src/test/scala/org/bulatnig/smpp/BufferSuite.scala
+++ src/test/scala/org/bulatnig/smpp/BufferSuite.scala
@@ -1,40 +1,214 @@
 package org.bulatnig.smpp
 
+import java.nio.charset.StandardCharsets
+
 import org.scalatest.FunSuite
+
+import scala.collection.mutable.ArrayBuffer
 
 class BufferSuite extends FunSuite {
 
   test("No argument constructor should create empty Buffer") {
-    val buffer = new Buffer()
-    assert(buffer.length == 0)
+    assert(new Buffer().toArray === new Array[Byte](0))
   }
 
-//  test("Byte append should work") {
-//    val buffer = new Buffer()
-//    val b = 255.toByte
-//    buffer += b
-//    println(b)
-//    println(buffer.toHexString)
-//  }
-
-  test("Long append should work") {
-    val buffer = new Buffer()
-    buffer += 4294967295L
-    assert(buffer.toHexString == "FFFFFFFF")
+  test("No argument constructor should create zero length Buffer") {
+    assert(new Buffer().length == 0)
   }
 
-  test("Long append should work 2") {
-    val buffer = new Buffer()
-    buffer += -1L
-    assert(buffer.toHexString == "FFFFFFFF")
+  test("Empty Buffer should generate empty HEX") {
+    assert(new Buffer().toHexString == "")
   }
 
-  test("read 1") {
+  test("Array[Byte] argument constructor should fill Buffer") {
+    val a = Array[Byte](1, 2, 3, 4)
+    val buffer = new Buffer(a)
+    assert(buffer.length == a.length)
+    assert(buffer.toArray === a)
+    assert(buffer.toHexString == "01020304")
+  }
+
+  test("ArrayBuffer[Byte] argument constructor should fill Buffer") {
+    val a = ArrayBuffer[Byte](1, 2, 3, 4)
+    val buffer = new Buffer(a)
+    assert(buffer.length == a.length)
+    assert(buffer.toArray === a)
+    assert(buffer.toHexString == "01020304")
+  }
+
+  test("Byte array append should work") {
+    val a = Array[Byte](1, 2, 3, 4)
     val buffer = new Buffer()
-    buffer += 255.toByte
+    buffer ++= a
+    assert(buffer.length == a.length)
+    assert(buffer.toArray === a)
+    assert(buffer.toHexString == "01020304")
+  }
+
+  test("Byte append should work") {
+    val buffer = new Buffer()
+    buffer.appendByte(128)
+    assert(buffer.toHexString == "80")
+  }
+
+  test("Byte read should work") {
+    val buffer = new Buffer()
+    buffer.appendByte(128)
+    assert(buffer.readByte() == 128)
+  }
+
+  test("Byte read out of bound should produce IndexOutOfBoundsException") {
+    val buffer = new Buffer()
+    buffer.appendByte(128)
+    buffer.readByte()
+    intercept[IndexOutOfBoundsException] {
+      new Buffer().readByte()
+    }
+  }
+
+  test("Unsigned byte max value should work") {
+    val buffer = new Buffer()
+    buffer.appendByte(255)
     assert(buffer.toHexString == "FF")
-    val result = buffer.readByte()
-    println(result)
+    assert(buffer.readByte() == 255)
+  }
+
+  test("Short append should work") {
+    val buffer = new Buffer()
+    buffer.appendShort(32768)
+    assert(buffer.toHexString == "8000")
+  }
+
+  test("Short read should work") {
+    val buffer = new Buffer()
+    buffer.appendShort(32768)
+    assert(buffer.readShort() == 32768)
+  }
+
+  test("Short read out of bound should produce IndexOutOfBoundsException") {
+    val buffer = new Buffer()
+    buffer.appendShort(50000)
+    buffer.readShort()
+    intercept[IndexOutOfBoundsException] {
+      new Buffer().readShort()
+    }
+  }
+
+  test("Unsigned short max value should work") {
+    val buffer = new Buffer()
+    buffer.appendShort(65535)
+    assert(buffer.toHexString == "FFFF")
+    assert(buffer.readShort() == 65535)
+  }
+
+  test("Int append should work") {
+    val buffer = new Buffer()
+    buffer.appendInt(-2147483648)
+    assert(buffer.toHexString == "80000000")
+  }
+
+  test("Int read should work") {
+    val buffer = new Buffer()
+    buffer.appendInt(-2147483648)
+    assert(buffer.readInt() == -2147483648)
+  }
+
+  test("Int read out of bound should produce IndexOutOfBoundsException") {
+    val buffer = new Buffer()
+    buffer.appendInt(12345)
+    buffer.readInt()
+    intercept[IndexOutOfBoundsException] {
+      new Buffer().readInt()
+    }
+  }
+
+  test("C-Octet String append should work") {
+    val buffer = new Buffer()
+    buffer.appendCString("smpp")
+    assert(buffer.toArray === Array[Byte](115, 109, 112, 112, 0))
+    assert(buffer.toHexString == "736D707000")
+  }
+
+  test("C-Octet String append null should work") {
+    val buffer = new Buffer()
+    buffer.appendCString(null)
+    assert(buffer.toArray === Array[Byte](0))
+    assert(buffer.toHexString == "00")
+  }
+
+  test("C-Octet String read should work") {
+    val s = "hello world"
+    val buffer = new Buffer()
+    buffer.appendCString(s)
+    assert(buffer.readCString() == s)
+  }
+
+  test("C-Octet String read null should work") {
+    val buffer = new Buffer()
+    buffer.appendCString(null)
+    assert(buffer.readCString() == null)
+  }
+
+  test("C-Octet String  read out of bound should produce IndexOutOfBoundsException") {
+    val buffer = new Buffer()
+    buffer.appendCString(null)
+    buffer.readCString()
+    intercept[IndexOutOfBoundsException] {
+      new Buffer().readCString()
+    }
+  }
+
+  test("Octet String append should work") {
+    val buffer = new Buffer()
+    buffer.appendString("Привет", StandardCharsets.UTF_16BE)
+    assert(buffer.toHexString == "041F04400438043204350442")
+  }
+
+  test("Octet String append null should work") {
+    val buffer = new Buffer()
+    buffer.appendString(null, StandardCharsets.UTF_16BE)
+    assert(buffer.toHexString == "")
+  }
+
+  test("Octet String read should work") {
+    val s = "Привет"
+    val buffer = new Buffer()
+    buffer.appendString(s, StandardCharsets.UTF_16BE)
+    assert(buffer.readString(12, StandardCharsets.UTF_16BE) == s)
+  }
+
+  test("Octet String read null should work") {
+    val buffer = new Buffer()
+    buffer.appendString(null, StandardCharsets.UTF_16BE)
+    assert(buffer.readString(0, StandardCharsets.UTF_16BE) == "")
+  }
+
+  test("Octet String  read out of bound should produce IndexOutOfBoundsException") {
+    val buffer = new Buffer()
+    buffer.appendString("smpp", StandardCharsets.US_ASCII)
+    buffer.readString(4, StandardCharsets.US_ASCII)
+    intercept[IndexOutOfBoundsException] {
+      new Buffer().readString(1, StandardCharsets.US_ASCII)
+    }
+  }
+
+  test("Append chaining should work") {
+    val buffer = new Buffer()
+    buffer.appendByte(1).appendShort(2).appendInt(3).appendCString("hello").appendString("Привет", StandardCharsets.UTF_16BE)
+    assert(buffer.length == 25)
+    assert(buffer.readByte() == 1)
+    assert(buffer.readShort() == 2)
+    assert(buffer.readInt() == 3)
+    assert(buffer.readCString() == "hello")
+    assert(buffer.readString(12, StandardCharsets.UTF_16BE) == "Привет")
+  }
+
+  test("rewind should let read buffer from the start") {
+    val buffer = new Buffer()
+    buffer.appendByte(128)
+    buffer.readByte()
+    buffer.rewind()
+    assert(buffer.readByte() == 128)
   }
 
 }
